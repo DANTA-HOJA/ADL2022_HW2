0e231426d372ec121dbd81504662017d4b05a69b
DANTA-HOJA <rime97410000@gmail.com>
2022-04-20 04:21:39 +0800

    version 3

--- run_MC.sh
+++ run_MC.sh
@@ -1,4 +1,5 @@
-export DATASET_NAME=MC_SaveDir
+# export DATASET_NAME=swag
+export DATASET_PATH=MC_SaveDir
 
 accelerate launch run_swag_no_trainer.py \
   --train_file ./Dataset/train.json \
@@ -8,9 +9,9 @@ accelerate launch run_swag_no_trainer.py \
   --max_length 256 \
   --per_device_train_batch_size 1 \
   --gradient_accumulation_steps 2 \
-  --num_train_epochs 2 \
+  --num_train_epochs 1 \
   --learning_rate 3e-5 \
-  --debug \
   --checkpointing_steps "epoch" \
-  --output_dir ./tmp/$DATASET_NAME/ 
+  --output_dir ./tmp/$DATASET_PATH/ \
+  --debug --debug_max_sample 10 \
   # --dataset_name $DATASET_NAME \
