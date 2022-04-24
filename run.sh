56696140789122bd3240d6aaf40155114b4e84a0
Ryan <rime97410000@gmail.com>
2022-04-25 06:32:16 +0800

    point to __Submit__test_version

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ echo -e "context file <= ${1}"
 echo -e "test file <= ${2}"
 echo -e "save to ${3}"
 
-unzip ./MC_task.zip
-unzip ./QA_task.zip
-bash ./test_MC.sh ${1} ${2}
-bash ./test_QA.sh ${1} ./QA_sheet.json ${3}
+unzip ./__Submit__test_version/MC_task.zip
+unzip ./__Submit__test_version/QA_task.zip
+bash ./__Submit__test_version/test_MC.sh ${1} ${2}
+bash ./__Submit__test_version/test_QA.sh ${1} ./QA_sheet.json ${3}
 
 echo -e "process complete"
