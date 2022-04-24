5d73625b5eeeb432d7424b56cac5f7a9c96eb18f
Ryan <rime97410000@gmail.com>
2022-04-25 06:26:45 +0800

    change dir : run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@ echo -e "context file <= ${1}"
 echo -e "test file <= ${2}"
 echo -e "save to ${3}"
 
+unzip ./MC_task.zip
+unzip ./QA_task.zip
 bash ./test_MC.sh ${1} ${2}
 bash ./test_QA.sh ${1} ./QA_sheet.json ${3}
 
