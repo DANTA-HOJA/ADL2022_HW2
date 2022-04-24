606d1e88b339065f5e989b0b0b80ac1a20c9ea8b
Ryan <rime97410000@gmail.com>
2022-04-25 07:10:52 +0800

    change dir

--- __Submit__test_version/run.sh
+++ __Submit__test_version/run.sh
@@ -2,13 +2,9 @@ echo -e "context file <= ${1}"
 echo -e "test file <= ${2}"
 echo -e "save to ${3}"
 
-cd __Submit__test_version
-
 unzip ./MC_task.zip
 unzip ./QA_task.zip
 bash ./test_MC.sh ${1} ${2}
 bash ./test_QA.sh ${1} ./QA_sheet.json ${3}
 
 echo -e "process complete"
-
-cd ..
--- download.sh
+++
@@ -1,3 +0,0 @@
-wget "https://ntucc365-my.sharepoint.com/:u:/g/personal/r10945018_ntu_edu_tw/ESzZ_E8_9PlHmM4eeQoZSccBrWRl-pkXpDeDnGtZNudGyw?e=BRm1GE&download=1" -O ./__Submit__test_version/MC_task.zip
-
-wget "https://ntucc365-my.sharepoint.com/:u:/g/personal/r10945018_ntu_edu_tw/Ea1mOLx14MNDsEkcH6SP9WMBo8P4O2SZWllK2GmKIwDP4w?e=MKfshb&download=1" -O ./__Submit__test_version/QA_task.zip
